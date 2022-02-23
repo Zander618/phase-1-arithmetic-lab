9079c32a13477328cb1d3e7bf2f5c45b0930fd5e
Zachary Sirna <zsirna@yahoo.com>
2022-02-22 20:44:22 -0500

    commit tree 8c243b2c659ce770f30b119d7076d8a3c83b33b7

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 22;
+const num4 = 6;
+
+let mod = num3 % num4;
+
+const max = Math.max(20);
